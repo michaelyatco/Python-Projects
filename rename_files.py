5a5493ef703bb0baf16433d176dc6aa58214a249
michaelyatco <mjyatco@gmail.com>
2017-06-30 15:13:07 -0400

    renaming files

---
+++ rename_files.py
@@ -0,0 +1,9 @@
+import os
+def rename_files():
+    file_list = os.listdir("/Users/mjyatco/Desktop/prank")
+    print(file_list)
+
+    for file_name in file_list:
+        os.rename(file_name, file_name.translate(None, "0123456789")
+        
+rename_files()
