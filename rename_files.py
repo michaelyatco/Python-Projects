cdc27f188c485a77ef380dbdfb262a355220e6a6
michaelyatco <mjyatco@gmail.com>
2017-06-30 15:30:45 -0400

    solved udacity exercise

--- rename_files.py
+++ rename_files.py
@@ -1,10 +1,14 @@
 import os
 def rename_files():
-    #file_list = os.listdir("/Users/mjyatco/Desktop/prank")
-    #print(file_list)
+    file_list = os.listdir("/Users/mjyatco/Desktop/prank")
+    print(file_list)
     saved_path = os.getcwd()
     print("Current Working Directory is "+saved_path)
-    #for file_name in file_list:
-        #os.rename(file_name, file_name.translate(None, "0123456789")
+    os.chdir("/Users/mjyatco/Desktop/prank")
+    for file_name in file_list:
+        print("Old Name - "+file_name)
+        print("New Name - "+file_name.translate(None, "0123456789"))
+        os.rename(file_name, file_name.translate(None, "0123456789"))
+    os.chdir(saved_path)
         
 rename_files()
