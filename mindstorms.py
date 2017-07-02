b119faa9ea41c09eceee9dcd5e6f8570ca78bbf6
michaelyatco <mjyatco@gmail.com>
2017-07-02 14:26:26 -0400

    Added triangle

--- mindstorms.py
+++ mindstorms.py
@@ -8,15 +8,33 @@ def draw_square():
     brad.shape("turtle")
     brad.color("pink")
     brad.speed(2)
-    brad.forward(100)
-    brad.right(90)
-    brad.forward(100)
-    brad.right(90)
-    brad.forward(100)
-    brad.right(90)
-    brad.forward(100)
-    brad.right(90)
 
-    window.exitonclick()
+    total_turns = 4
+    turn_count = 0
+
+    while (turn_count < total_turns):
+        brad.forward(100)
+        brad.right(90)
+        turn_count = turn_count + 1
+
+def draw_circle():
+    angie = turtle.Turtle()
+    angie.shape("arrow")
+    angie.color("yellow")
+    angie.circle(100)
+
+def draw_triangle():
+    zelda = turtle.Turtle()
+    zelda.shape("turtle")
+    zelda.color("blue")
+
+    total_turns = 3
+    turn_count = 0
+    while (turn_count < total_turns):
+        zelda.forward(100)
+        zelda.right(120)
+        turn_count = turn_count + 1
     
 draw_square()
+draw_circle()
+draw_triangle()
