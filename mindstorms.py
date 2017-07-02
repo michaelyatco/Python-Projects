2f66117c6a399c6bf7059585b90ac12b12bbf76f
michaelyatco <mjyatco@gmail.com>
2017-07-02 14:41:18 -0400

    Python loops

--- mindstorms.py
+++ mindstorms.py
@@ -12,13 +12,15 @@ def draw_art():
     brad = turtle.Turtle()
     brad.shape("turtle")
     brad.color("yellow")
-    brad.speed(2)
-    draw_square(brad)
+    brad.speed(7)
+    for i in range (1,37):
+        draw_square(brad)
+        brad.right(10)
     #Create the turtle Angie - Draws a circle
-    angie = turtle.Turtle()
-    angie.shape("arrow")
-    angie.color("blue")
-    angie.circle(100)
+    #angie = turtle.Turtle()
+    #angie.shape("arrow")
+    #angie.color("blue")
+    #angie.circle(100)
 
     window.exitonclick()
     
