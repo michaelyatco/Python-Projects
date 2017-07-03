bd28969348464defd3aed9d5b0ca6a1e49fc583f
michaelyatco <mjyatco@gmail.com>
2017-07-03 16:37:43 -0400

    Expanded movie list

--- movies/entertainment_center.py
+++ movies/entertainment_center.py
@@ -5,9 +5,38 @@ toy_story = media.Movie("Toy Story",
                         "http://www.gstatic.com/tv/thumb/movieposters/17420/p17420_p_v8_ab.jpg",
                         "https://www.youtube.com/watch?v=KYz2wyBy3kc")
 
-print(toy_story.storyline)
+#print(toy_story.storyline)
 avatar = media.Movie("Avatar",
                      "A marine on an alien planet",
                      "http://t0.gstatic.com/images?q=tbn:ANd9GcQCfmvrE4fMo2cd8esc7mDZPtFSJThAujddMPkRtti1_ij6u-jp",
                      "https://www.youtube.com/watch?v=5PSNL1qE6VY")
-print(avatar.storyline)
+#print(avatar.storyline)
+#avatar.show_trailer()
+
+assassins = media.Movie("13 Assassins",
+                           "13 samurai are tasked with assassinating a corrupt despot",
+                           "http://www.magnetreleasing.com/13assassins/images/photos/photo_09.jpg",
+                           "https://www.youtube.com/watch?v=NgPC74-Tde8")
+
+#assassins.show_trailer()
+
+midnight_in_paris = media.Movie("Midnight in Paris",
+                                "A man finds himself falling for nostalgia",
+                                "http://t3.gstatic.com/images?q=tbn:ANd9GcTk3ssys2bKM5-U6XMgvoD8yVoS5Io2YKg_1xA6x6GA8mKuuqID",
+                                "https://www.youtube.com/watch?v=FAfR8omt-CY")
+
+city_of_life_and_death = media.Movie("City of Life and Death",
+                                     "The fall of Nanjing and all its grisly, heartbreaking consequences",
+                                     "https://upload.wikimedia.org/wikipedia/en/thumb/2/29/CityofLifeandDeath.jpg/220px-CityofLifeandDeath.jpg",
+                                     "https://www.youtube.com/watch?v=SKEghDWe5SM")
+
+kung_fu_hustle = media.Movie("Kung Fu Hustle",
+                             "An action/comedy set in China that pays homage to cinematic masterpieces",
+                             "http://static.rogerebert.com/uploads/movie/movie_poster/kung-fu-hustle-2005/large_vSKFcFZ7Asvt9rON0glLD8FCKMU.jpg",
+                             "https://www.youtube.com/watch?v=-m3IB7N_PRk")
+
+
+
+
+
+
